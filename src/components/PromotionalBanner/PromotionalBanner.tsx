91a9b25a0b67aefe7d589c0491c640702868454f
Alejandro Nanez <alejonanez@gmail.com>
2021-07-11 07:38:49 -0500

    Created a `Did You Know` section on the Homepage.

--- src/components/PromotionalBanner/PromotionalBanner.tsx
+++ src/components/PromotionalBanner/PromotionalBanner.tsx
@@ -6,18 +6,18 @@ interface Props {
 
 export function PromotionalBanner({ datasetsSize }: Props) {
   return (
-    <div className="py-12 bg-white">
+    <div className="py-8 bg-white">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="lg:text-center">
           <h2 className="text-base text-indigo-600 font-semibold tracking-wide uppercase">
             Amazon Best Selling Books from the last decade
           </h2>
-          <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+          <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-5xl">
             The definitive analysis
           </p>
           <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
             We have analyzed a dataset of {datasetsSize} best selling books for
-            you!
+            you, so you don&#39;t have to!
           </p>
         </div>
       </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,19 +6,45 @@ import {
   getTotalBooksPerGenre,
 } from 'dataProcessing/data';
 import { bestSellers } from 'rawData/bestsellers';
-/* This example requires Tailwind CSS v2.0+ */
 import { PromotionalBanner } from 'components/PromotionalBanner/PromotionalBanner';
+import { Highlight, Text } from 'components/Typography/Typography';
+import { Section } from 'components/Layout/Section';
 
 export default function Home({
-  booksPerYear,
   datasetSize,
+  mostReviewedBook,
+  mostReviewsPerGenre,
+  totalBooksPerGenre,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <div>
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="max-w-3xl mx-auto">
           <PromotionalBanner datasetsSize={datasetSize} />
-          <pre>{JSON.stringify(booksPerYear, null, 2)}</pre>
+          <Section>
+            <Text>
+              💡 Did you know that the{' '}
+              <Highlight>most reviewed book was</Highlight>{' '}
+              <Highlight isLink>{mostReviewedBook.name}</Highlight>, by{' '}
+              <Highlight isLink>{mostReviewedBook.author}</Highlight> with{' '}
+              <Highlight>{mostReviewedBook.reviews}</Highlight> reviews.
+            </Text>
+            <Text>
+              Also, <Highlight isLink>Fiction books</Highlight> got{' '}
+              <Highlight>{mostReviewsPerGenre.fiction}</Highlight> total
+              reviews, and <Highlight isLink>Non-Fiction</Highlight> got{' '}
+              <Highlight>{mostReviewsPerGenre.nonFiction}</Highlight> total
+              reviews from 2009 and 2019.
+            </Text>
+            <Text>
+              Another fun fact, is that{' '}
+              <Highlight>{totalBooksPerGenre.fiction}</Highlight> out of the{' '}
+              <Highlight>{datasetSize}</Highlight> Bestsellers were{' '}
+              <Highlight isLink>Fiction books</Highlight> and the remaining{' '}
+              <Highlight>{totalBooksPerGenre.nonFiction}</Highlight> were{' '}
+              <Highlight isLink>Non-Fiction books</Highlight>
+            </Text>
+          </Section>
         </div>
       </div>
     </div>
