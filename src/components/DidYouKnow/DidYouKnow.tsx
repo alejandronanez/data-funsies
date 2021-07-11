98390111ed56a2be6ca7621b656331a990672db3
Alejandro Nanez <alejonanez@gmail.com>
2021-07-11 07:56:38 -0500

    Formatted numbers in DidYouKnow section

--- src/components/DidYouKnow/DidYouKnow.tsx
+++ src/components/DidYouKnow/DidYouKnow.tsx
@@ -1,4 +1,5 @@
 import { Highlight, Text } from 'components/Typography/Typography';
+import { formatNumbersToLocaleString } from 'utils/numbers';
 
 export function DidYouKnow({
   bookName,
@@ -25,21 +26,31 @@ export function DidYouKnow({
         💡 Did you know that the <Highlight>most reviewed book was</Highlight>{' '}
         <Highlight isLink>{bookName}</Highlight>, by{' '}
         <Highlight isLink>{author}</Highlight> with{' '}
-        <Highlight>{totalReviews}</Highlight> reviews.
+        <Highlight>{formatNumbersToLocaleString(totalReviews)}</Highlight>{' '}
+        reviews.
       </Text>
       <Text>
         Also, <Highlight isLink>Fiction books</Highlight> got{' '}
-        <Highlight>{fictionTotalReviews}</Highlight> total reviews, and{' '}
-        <Highlight isLink>Non-Fiction</Highlight> got{' '}
-        <Highlight>{nonFictionTotalReviews}</Highlight> total reviews from 2009
-        and 2019.
+        <Highlight>
+          {formatNumbersToLocaleString(fictionTotalReviews)}
+        </Highlight>{' '}
+        total reviews, and <Highlight isLink>Non-Fiction</Highlight> got{' '}
+        <Highlight>
+          {formatNumbersToLocaleString(nonFictionTotalReviews)}
+        </Highlight>{' '}
+        total reviews from 2009 to 2019.
       </Text>
       <Text>
-        Another fun fact, is that <Highlight>{fictionBestSellers}</Highlight>{' '}
-        out of the <Highlight>{totalBooks}</Highlight> Bestsellers were{' '}
-        <Highlight isLink>Fiction books</Highlight> and the remaining{' '}
-        <Highlight>{nonFictionBestSellers}</Highlight> were{' '}
-        <Highlight isLink>Non-Fiction books</Highlight>
+        Another fun fact, is that{' '}
+        <Highlight>{formatNumbersToLocaleString(fictionBestSellers)}</Highlight>{' '}
+        out of the{' '}
+        <Highlight>{formatNumbersToLocaleString(totalBooks)}</Highlight>{' '}
+        Bestsellers were <Highlight isLink>Fiction books</Highlight> and the
+        remaining{' '}
+        <Highlight>
+          {formatNumbersToLocaleString(nonFictionBestSellers)}
+        </Highlight>{' '}
+        were <Highlight isLink>Non-Fiction books</Highlight>
       </Text>
     </>
   );
