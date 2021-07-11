5c12004e6827ecff3c98e3ae106dffcd39fe06d6
Alejandro Nanez <alejonanez@gmail.com>
2021-07-11 07:46:08 -0500

    Add relevant title for DataFunsies

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,7 +5,9 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>Title - MAKE ME DYNAMIC PLEASE</title>
+        <title>
+          Data Funsies - A fun data analysis of 550 best selling books
+        </title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />;
       </Head>
