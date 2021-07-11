85ba07a53214d29dfb5a386e3302d20a1031f89b
Alejandro Nanez <alejonanez@gmail.com>
2021-07-10 20:53:17 -0500

    Created the promotional banner.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,22 @@
 import { InferGetStaticPropsType } from 'next';
 import { getBooksPerYear } from 'dataProcessing/data';
 import { bestSellers } from 'rawData/bestsellers';
+/* This example requires Tailwind CSS v2.0+ */
+import { PromotionalBanner } from 'components/PromotionalBanner/PromotionalBanner';
 
 export default function Home({
   booksPerYear,
+  datasetSize,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
-    <>
-      <pre>{JSON.stringify(booksPerYear, null, 2)}</pre>
-    </>
+    <div>
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="max-w-3xl mx-auto">
+          <PromotionalBanner datasetsSize={datasetSize} />
+          <pre>{JSON.stringify(booksPerYear, null, 2)}</pre>
+        </div>
+      </div>
+    </div>
   );
 }
 export const getStaticProps = async () => {
@@ -21,6 +29,7 @@ export const getStaticProps = async () => {
   return {
     props: {
       booksPerYear: getBooksPerYear(bestSellers),
+      datasetSize: bestSellers.length,
     },
   };
 };
