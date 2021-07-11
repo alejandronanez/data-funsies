6f65833371d5a3e7d0db7a5e829391ec5afb73f5
Alejandro Nanez <alejonanez@gmail.com>
2021-07-10 20:53:09 -0500

    Updated transformers and a few types

--- src/dataProcessing/transformers.ts
+++ src/dataProcessing/transformers.ts
@@ -1,5 +1,10 @@
-import { groupBy, countBy } from 'lodash';
-import { BestSeller, BookGenreIndexes, YearIndexes } from 'types/types';
+import { groupBy, countBy, entries } from 'lodash';
+import {
+  BestSeller,
+  BookGenreIndexes,
+  Years,
+  YearValuePair,
+} from 'types/types';
 
 export function groupBooksByGenre(
   books: BestSeller[],
@@ -12,6 +17,13 @@ export function groupBooksByGenre(
   };
 }
 
-export function getTotalBooksPerYear(books: BestSeller[]): YearIndexes<number> {
-  return countBy(books, 'year');
+export function getTotalBooksPerYear(books: BestSeller[]): YearValuePair[] {
+  const booksPerYear = countBy(books, 'year');
+
+  return entries(booksPerYear).map(([year, value]) => ({
+    // TS complains about the fact that `year` is not a number anymore as it's
+    // coming from an object, so it assumes the key is a string.
+    year: +year as Years,
+    value,
+  }));
 }
--- src/types/types.ts
+++ src/types/types.ts
@@ -1,6 +1,6 @@
 export type BookGenre = 'nonFiction' | 'fiction';
 export type BookGenreIndexes<T> = { [Property in BookGenre]?: T };
-export type YearItBecameBestSeller =
+export type Years =
   | 2009
   | 2010
   | 2011
@@ -13,13 +13,15 @@ export type YearItBecameBestSeller =
   | 2018
   | 2019;
 
-export type YearIndexes<T> = { [Property in YearItBecameBestSeller]?: T };
+export type YearIndexes<T> = { [Property in Years]?: T };
 export interface BestSeller {
   name: string;
   author: string;
   userRating: number;
   reviews: number;
   price: number;
-  year: YearItBecameBestSeller;
+  year: Years;
   genre: BookGenre;
 }
+
+export type YearValuePair = { year: Years; value: number };
